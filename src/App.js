6a207c20b8e439a04b4c7c2611fcd54061e820f5
Yerimah <danielyerimah@yahoo.com>
2022-07-04 14:21:48 +0100

    Create react app and install neccessary dependencies

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Navbar from './components/Nav/Navbar';
+
+const App = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+       <Navbar />
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
