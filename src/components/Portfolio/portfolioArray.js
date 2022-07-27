9995bbfaa5a758fd66a37325b21ce6f10e6353a3
Yerimah <danielyerimah@yahoo.com>
2022-07-27 13:58:58 +0100

    Add portfolio data

---
+++ src/components/Portfolio/portfolioArray.js
@@ -0,0 +1,42 @@
+const projects = [
+    {
+        title: 'Stock App',
+        description: 'Stock metrics is a web(mobile-focused) app for checking the companies listed on the stock exchange and see their live metrics.',
+        stack: [{t: 'React', i: './icons/react.svg'}, {t: 'Redux', i: './icons/redux.svg'}],
+        img: './img/seispalabras.png',
+        visit: 'https://stockmetricapp.netlify.app/',
+        demo: null,
+        repo: 'https://github.com/Yerimah/stock-metrics'
+
+    },
+    {
+        title: 'Budgeti App',
+        description: 'Budgeti is a budget tracking app that helps you monitor how your budget was allocated and spent. Budgeti is built with Ruby on Rails and styled using boostrap.',
+        stack: [{t: 'Ruby', i: './icons/ruby.svg'}, {t: 'Rails', i: './icons/rubyonrails.svg'}],
+        img: './img/seispalabras.png',
+        visit: 'https://intense-harbor-49660.herokuapp.com/',
+        demo: null,
+        repo: 'https://github.com/Yerimah/Budgeti'
+
+    },
+    {
+        title: 'Cinemash',
+        description: 'The cinemash app is an online curation of blockbuster movies.',
+        stack: [{t: 'React', i: './icons/react.svg'}, {t: 'Redux', i: './icons/redux.svg'}],
+        img: './img/seispalabras.png',
+        visit: 'https://cinemash.netlify.app/',
+        demo: null,
+        repo: 'https://github.com/Yerimah/JS-Capstone-Project'
+
+    },
+    {
+        title: 'Fresh Start',
+        description: 'Web app where Spanish students can practice translating six words every day. The students are able to create an account and keep track of their progress.',
+        stack: [{t: 'JavaScript', i: './icons/javascript.svg'}, {t: 'HTML', i: './icons/html.svg'}, {t: 'css', i: './icons/css.svg'}],
+        img: './img/seispalabras.png',
+        visit: 'https://yerimah.github.io/Capstone-project/',
+        demo: null,
+        repo: 'https://github.com/Yerimah/Capstone-project'
+
+    },
+]
